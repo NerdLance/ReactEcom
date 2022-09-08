1c0073f8e1e27aaf821023ddf81cbcf17b71cc5d
NerdLance <101930566+NerdLance@users.noreply.github.com>
2022-09-08 10:32:00 -0400

    Added Persist Cart Support using Redux Persist.

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,30 @@
 // import { compose, applyMiddleware } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import { logger } from 'redux-logger'
 
 import { rootReducer } from './root-reducer'
 
-const middleWares = [logger]
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
 // const composedEnhancers = compose(applyMiddleware(...middleWares))
 
 // export const store = createStore(rootReducer, undefined, composedEnhancers)
 
 const storeOptions = {
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: middleWares
 }
 
 export const store = configureStore(storeOptions)
+
+export const persistor = persistStore(store)
